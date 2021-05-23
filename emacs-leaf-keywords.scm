cc2dce7243be1bbea9f8c5ffa9fd0315c28440fd
Zain Jabbar <zaijab2000@gmail.com>
2021-05-22 19:31:55 -1000

    Trying to use emacs-xyz for emacs-leaf input.

---
+++ emacs-leaf-keywords.scm
@@ -0,0 +1,28 @@
+(define-module (zaijab packages emacs-leaf-keywords)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system emacs)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix package emacs-xyz))
+
+(define-public emacs-leaf-keywords
+  (package
+   (name "emacs-leaf-keywords")
+   (version "master")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/conao3/leaf-keywords.el.git")
+		  (commit version)))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32
+	      "16iv1cvlky2gij1ndx2d6q8l35axm72bx52n6v5y3h21aibj197n"))))
+   (build-system emacs-build-system)
+   (propagated-inputs
+    `(("emacs-leaf" ,emacs-leaf)))
+   (home-page "https://github.com/conao3/leaf-keywords.el")
+   (synopsis "Extra keywords for leaf.")
+   (description "Extra keywords for leaf.")
+   (license license:gpl3+)))
