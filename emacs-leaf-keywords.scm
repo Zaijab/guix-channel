6ea9e0acfed0a382a941a7df1184335e2f724389
Zain Jabbar <zaijab2000@gmail.com>
2021-05-22 19:14:21 -1000

    Redefining package to be a module in gnu packages namespace and to be a public symbol.

--- emacs-leaf-keywords.scm
+++ emacs-leaf-keywords.scm
@@ -1,26 +1,26 @@
-(use-modules
- (guix packages)
- (guix git-download)
- (guix build-system emacs)
- ((guix licenses) #:prefix license:)
- (gnu packages emacs-xyz))
+(define-module (gnu packages emacs-leaf-keywords)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system emacs)
+  #:use-module ((guix licenses) #:prefix license:))
 
-(package
- (name "emacs-leaf-keywords")
- (version "master")
- (source (origin
-	  (method git-fetch)
-	  (uri (git-reference
-		(url "https://github.com/conao3/leaf-keywords.el.git")
-		(commit version)))
-	  (file-name (git-file-name name version))
-	  (sha256
-	   (base32
-	    "16iv1cvlky2gij1ndx2d6q8l35axm72bx52n6v5y3h21aibj197n"))))
- (build-system emacs-build-system)
- (propagated-inputs
-  `(("emacs-leaf" ,emacs-leaf)))
- (home-page "https://github.com/conao3/leaf-keywords.el")
- (synopsis "Extra keywords for leaf.")
- (description "Extra keywords for leaf.")
- (license license:gpl3+))
+(define-public emacs-leaf-keywords
+  (package
+   (name "emacs-leaf-keywords")
+   (version "master")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://github.com/conao3/leaf-keywords.el.git")
+		  (commit version)))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32
+	      "16iv1cvlky2gij1ndx2d6q8l35axm72bx52n6v5y3h21aibj197n"))))
+   (build-system emacs-build-system)
+   (propagated-inputs
+    `(("emacs-leaf" ,emacs-leaf)))
+   (home-page "https://github.com/conao3/leaf-keywords.el")
+   (synopsis "Extra keywords for leaf.")
+   (description "Extra keywords for leaf.")
+   (license license:gpl3+)))
