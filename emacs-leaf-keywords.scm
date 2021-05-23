f77823fcddedc915ec9e0bf2199f26f030baf0ad
Zain Jabbar <zaijab2000@gmail.com>
2021-05-22 20:06:12 -1000

    Using emacs-leaf definition from main channel.

--- emacs-leaf-keywords.scm
+++ emacs-leaf-keywords.scm
@@ -9,35 +9,35 @@
   #:use-module (gnu packages emacs-xyz))
 
 
-(define-public emacs-leaf
-  (package
-    (name "emacs-leaf")
-    (version "3.5.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/conao3/leaf.el.git")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "1rgd59146wad92yc64las0qgx67k2ifgsw1vwhp40xvkd7kb0r6d"))))
-    (build-system emacs-build-system)
-    (home-page "https://github.com/conao3/leaf.el")
-    (arguments
-     `(#:tests? #t
-       #:test-command '("emacs" "--batch"
-                        "-l" "leaf-tests.el"
-                        "-f" "cort-test-run")))
-    (synopsis
-     "Simplify your init.el configuration, extended use-package")
-    (description
-     "This package provides macros that allows you to declaratively configure
-settings typical of an Elisp package with various keywords.  The syntax is
-similar, but not identical to use-package -- overall, leaf aims at a cleaner
-and more predictable implementation.")
-    (license license:agpl3+)))
+;; (define-public emacs-leaf
+;;   (package
+;;     (name "emacs-leaf")
+;;     (version "3.5.0")
+;;     (source
+;;      (origin
+;;        (method git-fetch)
+;;        (uri (git-reference
+;;              (url "https://github.com/conao3/leaf.el.git")
+;;              (commit (string-append "v" version))))
+;;        (file-name (git-file-name name version))
+;;        (sha256
+;;         (base32
+;;          "1rgd59146wad92yc64las0qgx67k2ifgsw1vwhp40xvkd7kb0r6d"))))
+;;     (build-system emacs-build-system)
+;;     (home-page "https://github.com/conao3/leaf.el")
+;;     (arguments
+;;      `(#:tests? #t
+;;        #:test-command '("emacs" "--batch"
+;;                         "-l" "leaf-tests.el"
+;;                         "-f" "cort-test-run")))
+;;     (synopsis
+;;      "Simplify your init.el configuration, extended use-package")
+;;     (description
+;;      "This package provides macros that allows you to declaratively configure
+;; settings typical of an Elisp package with various keywords.  The syntax is
+;; similar, but not identical to use-package -- overall, leaf aims at a cleaner
+;; and more predictable implementation.")
+;;     (license license:agpl3+)))
 
 (define-public emacs-leaf-keywords
   (package
@@ -53,8 +53,8 @@ and more predictable implementation.")
 	     (base32
 	      "16iv1cvlky2gij1ndx2d6q8l35axm72bx52n6v5y3h21aibj197n"))))
    (build-system emacs-build-system)
-   (propagated-inputs
-    `(("emacs-leaf" ,emacs-leaf)))
+   ;; (propagated-inputs
+   ;;  `(("emacs-leaf" ,emacs-leaf)))
    (home-page "https://github.com/conao3/leaf-keywords.el")
    (synopsis "Extra keywords for leaf.")
    (description "Extra keywords for leaf.")
