1e01360ccfbc47fd3c2a78a11ea89f2703de7fbc
Zain Jabbar <zaijab2000@gmail.com>
2021-05-22 20:27:14 -1000

    Adding 'emacs-system-packages' to the mix.

--- zaijab/packages/emacs-xyz.scm
+++ zaijab/packages/emacs-xyz.scm
@@ -1,4 +1,4 @@
-(define-module (zaijab packages emacs-leaf-keywords)
+(define-module (zaijab packages emacs-xyz)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
@@ -26,3 +26,24 @@
    (synopsis "Extra keywords for leaf.")
    (description "Extra keywords for leaf.")
    (license license:gpl3+)))
+
+(define-public emacs-system-packages
+  (package
+   (name "emacs-system-packages")
+   (version "1.0.11")
+   (source (origin
+	    (method git-fetch)
+	    (uri (git-reference
+		  (url "https://gitlab.com/jabranham/system-packages.git")
+		(commit version)))
+	    (file-name (git-file-name name version))
+	    (sha256
+	     (base32
+	      "0pxkyys2lgn16rhf4mzqlh27vs9aw6g083z2vr2agr7bmbavd2fp"))))
+   (build-system emacs-build-system)
+ (home-page "https://gitlab.com/jabranham/system-packages")
+ (synopsis "Emacs interface to system package manager.")
+ (description "Use Emacs to install packages using system package manager.")
+ (license license:gpl3+)))
+
+
