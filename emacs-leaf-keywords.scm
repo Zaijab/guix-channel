2e5a74a131233d7f7caa217aee7c8b5aa8e4b097
Zain Jabbar <zaijab2000@gmail.com>
2021-05-22 19:37:52 -1000

    Using emacs-leaf definition from main channel.

--- emacs-leaf-keywords.scm
+++ emacs-leaf-keywords.scm
@@ -26,3 +26,33 @@
    (synopsis "Extra keywords for leaf.")
    (description "Extra keywords for leaf.")
    (license license:gpl3+)))
+
+(define-public emacs-leaf
+  (package
+    (name "emacs-leaf")
+    (version "3.5.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/conao3/leaf.el.git")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1rgd59146wad92yc64las0qgx67k2ifgsw1vwhp40xvkd7kb0r6d"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/conao3/leaf.el")
+    (arguments
+     `(#:tests? #t
+       #:test-command '("emacs" "--batch"
+                        "-l" "leaf-tests.el"
+                        "-f" "cort-test-run")))
+    (synopsis
+     "Simplify your init.el configuration, extended use-package")
+    (description
+     "This package provides macros that allows you to declaratively configure
+settings typical of an Elisp package with various keywords.  The syntax is
+similar, but not identical to use-package -- overall, leaf aims at a cleaner
+and more predictable implementation.")
+    (license license:agpl3+)))
