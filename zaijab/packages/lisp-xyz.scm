f513922af23d2212c2eb7ed099887cf45ed16243
Zain Jabbar <zaijab2000@gmail.com>
2022-03-04 20:22:44 -1000

    Fixing sdl2 host file-name

--- zaijab/packages/lisp-xyz.scm
+++ zaijab/packages/lisp-xyz.scm
@@ -15,7 +15,7 @@
 	      (uri (git-reference
 		    (url "https://github.com/cbaggers/cepl.sdl2.git")
 		    (commit version)))
-	      (file-name (git-file-name "cepl.sdl2" version))
+	      (file-name "cepl.sdl2")
 	      (sha256
 	       (base32
 		"0lz8yxm1g2ch0w779lhrs2xkfciy3iz6viz7cdgyd2824isvinjf"))))
