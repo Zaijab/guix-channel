f8750841343b8dc7262a1acb09701357708e78ba
Zain Jabbar <zaijab2000@gmail.com>
2022-11-24 17:15:11 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -7,6 +7,7 @@
   #:use-module (nongnu packages linux)
   #:use-module (gnu)
   #:use-module (gnu packages)
+  #:use-module (gnu packages linux)
   #:use-module (gnu services base)
   #:use-module (gnu services admin)
   #:use-module (gnu services desktop)
