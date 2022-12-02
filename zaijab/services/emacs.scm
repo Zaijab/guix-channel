13147c4ac90a089cdc9969de506e9ee9a3e4a52d
Zain Jabbar <zaijab2000@gmail.com>
2022-12-02 12:18:12 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -882,9 +882,7 @@ This is mainly to override org-roam's default filename convention of `timestamp-
 (define home-emacs-total-configuration
   (fold (lambda (config-1 config-2) (home-emacs-configuration
 				     (emacs
-				      ((options->transformation
-					'((with-branch . "emacs-next=emacs-29")))
-				       (specification->package "emacs-next")))
+				     (specification->package "emacs-next"))
 				     (init (append (home-emacs-configuration-init config-1)
 						   (home-emacs-configuration-init config-2)))
 				     (early-init (append (home-emacs-configuration-early-init config-1)
