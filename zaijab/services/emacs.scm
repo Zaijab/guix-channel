886b115666db8f9f7b2f580503a1f9d5871c61db
Zain Jabbar <zaijab2000@gmail.com>
2022-12-18 11:30:20 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1012,7 +1012,9 @@
 
 (define home-emacs-total-configuration
   (fold (lambda (config-1 config-2) (home-emacs-configuration
-				     (emacs (specification->package "emacs-next"))
+				     (emacs ((options->transformation '((with-branch . "emacs-next=master")
+									(with-patch . "emacs-next=/home/zjabbar/code/guix-channel/zaijab/services/emacs-exec-path.patch")
+									)) (specification->package "emacs-next"))) 
 				     (init (append (home-emacs-configuration-init config-1)
 						   (home-emacs-configuration-init config-2)))
 				     (early-init (append (home-emacs-configuration-early-init config-1)
