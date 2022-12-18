82fe6d64eb4aa0b6798940d79e83e30acecc088d
Zain Jabbar <zaijab2000@gmail.com>
2022-12-17 22:00:39 -1000

    Updating Config

--- zaijab/packages/emacs-xyz.scm
+++ zaijab/packages/emacs-xyz.scm
@@ -311,34 +311,54 @@ layer that explicitly uses the abstract syntax tree, for greater precision.")
 (define-public emacs-elfeed-tube
   (package
     (name "emacs-elfeed-tube")
-    (version "master")
+    (version "20220823.238")
     (source (origin
 	      (method git-fetch)
 	      (uri (git-reference
 		    (url "https://github.com/karthink/elfeed-tube.git")
-		    (commit version)))
-	      (file-name (git-file-name name version))
+		    (commit "18d89f19203423b9e2df59a556c1240746903d8f")))
 	      (sha256
 	       (base32
 		"0h32bpq8w1j3rbwfqlfj10vvlw21j9rcpqjpp45pw8z6xxb2q0lp"))))
     (build-system emacs-build-system)
-    (inputs
-     `(("mpv" ,mpv)))
-    (propagated-inputs
-     `(("emacs-elfeed" ,emacs-elfeed)
-       ("emacs-aio" ,emacs-aio)
-       ("emacs-mpv-el" ,emacs-mpv-el)
-       ("mpv" ,mpv)
-       ("curl" ,curl)))
-    (home-page "")
-    (synopsis "")
-    (description "")
-    (license license:gpl3+)))
-
-
-
-
-
+    (propagated-inputs (list emacs-elfeed emacs-aio))
+    (arguments
+     '(#:include '("^elfeed-tube.el$" "^elfeed-tube-utils.el$"
+		   "^elfeed-tube-fill.el$")
+       #:exclude '()))
+    (home-page "https://github.com/karthink/elfeed-tube")
+    (synopsis "YouTube integration for Elfeed")
+    (description
+     "Elfeed Tube is an extension for Elfeed, the feed reader for Emacs, that en
+hances
+your Youtube RSS feed subscriptions.  Typically Youtube RSS feeds contain only
+the title and author of each video.  Elfeed Tube adds video descriptions,
+thumbnails, durations, chapters and \"live\" transcrips to video entries.  See
+https://github.com/karthink/elfeed-tube for demos.  This information can
+optionally be added to your entry in your Elfeed database.  The displayed
+transcripts and chapter headings are time-aware, so you can click on any
+transcript segment to visit the video at that time (in a browser or your video
+player if you also have youtube-dl).  A companion package, `elfeed-tube-mpv',
+provides complete mpv (video player) integration with the transcript, including
+video seeking through the transcript and following along with the video in
+Emacs.  To use this package, (i) Subscribe to Youtube channel or playlist feeds
+in Elfeed.  You can use the helper function `elfeed-tube-add-feeds provided by
+this package to search for Youtube channels by URLs or search queries. (ii)
+Place in your init file the following: (require elfeed-tube) (elfeed-tube-setup)
+(iii) Use Elfeed as normal, typically with `elfeed'.  Your Youtube feed entries
+should be fully populated.  You can also call `elfeed-tube-fetch in an Elfeed
+buffer to manually populate an entry, or obtain an Elfeed entry-like summary for
+ANY youtube video (no subscription needed) by manually calling
+`elfeed-tube-fetch from outside Elfeed.  User options: There are three options
+of note: `elfeed-tube-fields': Customize this to set the kinds of metadata you
+want added to Elfeed's Youtube entries.  You can selectively turn on/off
+thumbnails, transcripts etc. `elfeed-tube-auto-save-p': Set this boolean to save
+fetched Youtube metadata to your Elfeed database, i.e.  to persist the data on
+disk for all entries. `elfeed-tube-auto-fetch-p': Unset this boolean to turn off
+fetching metadata.  You can then call `elfeed-tube-fetch to manually fetch data
+for specific feed entries.  See the customization group `elfeed-tube for more
+options.  See the README for more information.")
+    (license #f)))
 
 
 
