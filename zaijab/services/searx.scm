c02012b3366c3e4ca7339f3193839b3b3e37ceff
Zain Jabbar <zaijab2000@gmail.com>
2022-12-22 15:45:57 -1000

    Updating Config

--- zaijab/services/searx.scm
+++ zaijab/services/searx.scm
@@ -4,26 +4,26 @@
   #:use-module (gnu packages search)
   #:use-module (guix gexp))
 
-(define (searx-profile-service config)
-  (list searx))
+;; (define (searx-profile-service config)
+;;   (list searx))
 
-(define (searx-shepherd-service config)
-  (list (shepherd-service
-	 (provision '(searx))
-	 (documentation "Run and control searx daemon.")
-	 (start #~(make-forkexec-constructor (list #$(file-append searx "/bin/searx-run"))
-					     #:log-file "/home/zjabbar/.local/var/log/searx.log" h))
-	 (stop #~(make-kill-destructor)))))
+;; (define (searx-shepherd-service config)
+;;   (list (shepherd-service
+;; 	 (provision '(searx))
+;; 	 (documentation "Run and control searx daemon.")
+;; 	 (start #~(make-forkexec-constructor (list #$(file-append searx "/bin/searx-run"))
+;; 					     #:log-file "/home/zjabbar/.local/var/log/searx.log" h))
+;; 	 (stop #~(make-kill-destructor)))))
 
-(define-public searx-service-type
-  (service-type (name 'searx)
-                (extensions
-                 (list (service-extension
-                        profile-service-type
-                        searx-profile-service)
-		       (service-extension
-			shepherd-service-type
-			searx-shepherd-service)))
-                (default-value #f)
-                (description "Searx Time.")))
+;; (define-public searx-service-type
+;;   (service-type (name 'searx)
+;;                 (extensions
+;;                  (list (service-extension
+;;                         profile-service-type
+;;                         searx-profile-service)
+;; 		       (service-extension
+;; 			shepherd-service-type
+;; 			searx-shepherd-service)))
+;;                 (default-value #f)
+;;                 (description "Searx Time.")))
 
