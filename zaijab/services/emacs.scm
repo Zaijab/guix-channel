eff241066ecd78b81e0f4fdcee269b4fc8aa6993
Zain Jabbar <zaijab2000@gmail.com>
2023-01-05 16:05:59 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -191,8 +191,9 @@
 	     (interactive)
 	     (let ((inhibit-read-only t))
 	       (ansi-color-apply-on-region (point-min) (point-max))))
-	   (add-hook 'compilation-mode-hook 'colorize-compilation-buffer)
-	   (add-hook 'org-mode-hook 'colorize-compilation-buffer)))))
+	   (add-hook 'compilation-mode-hook (function colorize-compilation-buffer))
+	   (add-hook 'org-mode-hook (function colorize-compilation-buffer))
+	   ))))
 
 (define language-configuration
   (home-emacs-configuration
