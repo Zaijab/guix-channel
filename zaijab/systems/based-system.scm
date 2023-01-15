e5d23111c4b82a3001a36001613d06120c3f3434
Zain Jabbar <zaijab2000@gmail.com>
2023-01-14 16:45:24 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -132,3 +132,20 @@
 			       (specification->package "network-manager-openconnect")
 			       (specification->package "openconnect-sso"))))))))))
 
+
+(define-public euler-operating-system
+  (operating-system
+    (inherit tao-operating-system)
+    (host-name "euler")
+    (file-systems (cons*
+		   (file-system
+		     (mount-point "/boot/efi")
+		     (device (uuid "A2B7-72DF" 'fat32))
+		     (type "vfat"))
+		   (file-system
+		     (mount-point "/")
+		     (device
+		      (uuid "08173b91-2416-4b98-a5e1-59281ae236a2"
+			    'btrfs))
+		     (type "btrfs"))
+		   %base-file-systems))))
