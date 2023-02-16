128e5841366101df1b8129e0d3fadfc82bbeda7a
Zain Jabbar <zaijab2000@gmail.com>
2023-02-15 17:57:02 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -780,7 +780,7 @@
 	      ((options->transformation		
 		'((with-branch . "emacs-jupyter=master")
 		  (with-branch . "emacs-zmq=master")
-		  (with-branch . "zeromq=master")))
+		  (with-source . "zeromq=https://github.com/zeromq/libzmq.git")))
 	       (specification->package "emacs-jupyter"))
 	      (specification->package "pandoc")
 	      (specification->package "python-sqlalchemy")
