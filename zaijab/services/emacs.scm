c8c029ceba1af8787344a19260416162a39660f2
Zain Jabbar <zaijab2000@gmail.com>
2023-02-15 18:20:09 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -777,11 +777,8 @@
 	      (specification->package "emacs-csv-mode")
 	      (specification->package "emacs-py-isort")
 	      emacs-py-autopep8
-	      ;; ((options->transformation		
-	      ;; 	'((with-branch . "emacs-jupyter=master")))
-	      ;;  (specification->package "emacs-jupyter"))
-	      ((options->transformation
-		'((with-source . "emacs-jupyter=https://github.com/tgbugs/emacs-jupyter")))
+	      ((options->transformation		
+		'((with-branch . "emacs-jupyter=master")))
 	       (specification->package "emacs-jupyter"))
 	      (specification->package "pandoc")
 	      (specification->package "python-sqlalchemy")
