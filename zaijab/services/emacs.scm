c636fa942c0cd066c4fe6b764df1c86386668020
Zain Jabbar <zaijab2000@gmail.com>
2023-04-14 23:10:09 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -989,7 +989,57 @@ and sends a message of the current volume status."
    (packages (list (specification->package "emacs-vterm")
 		   (specification->package "postgresql")
 		   (specification->package "sqls")))
-   (init '(
+   (init '((add-to-list 'eglot-server-programs '(sql-mode . ("sqls")))
+
+	   (setq eglot-sync-connect 1)
+
+	   (add-to-list 'display-buffer-alist
+			'("\\*sqls\\*"
+			  (display-buffer-reuse-window display-buffer-at-bottom)
+			  (reusable-frames . visible)
+			  (window-height . 0.3)))
+
+	   (defclass eglot-sqls (eglot-lsp-server) () :documentation "SQL's Language Server")
+					;(add-to-list 'eglot-server-programs '(sql-mode . (eglot-sqls "sqls")))
+	   (cl-defmethod eglot-execute-command
+			 ((server eglot-sqls) (command (eql executeQuery)) arguments)
+			 "For executeQuery."
+			 ;; (ignore-errors
+			 (let* ((beg (eglot--pos-to-lsp-position (if (use-region-p) (region-beginning) (point-min))))
+				(end (eglot--pos-to-lsp-position (if (use-region-p) (region-end) (point-max))))
+				(res (jsonrpc-request server :workspace/executeCommand
+						      `(:command ,(format "%s" command) :arguments ,arguments
+							:timeout 0.5 :range (:start ,beg :end ,end))))
+				(buffer (generate-new-buffer "*sqls*")))
+			   (with-current-buffer buffer
+						(eglot--apply-text-edits `[
+									   (:range
+									    (:start
+									     (:line 0 :character 0)
+									     :end
+									     (:line 0 :character 0))
+									    :newText ,res)
+									   ]
+									 )
+						(org-mode))
+			   (pop-to-buffer buffer))
+			 )
+	   (cl-defmethod eglot-execute-command
+			 ((server eglot-sqls) (_cmd (eql switchDatabase)) arguments)
+			 "For switchDatabase."
+			 (let* ((res (jsonrpc-request server :workspace/executeCommand
+						      `(:command "showDatabases" :arguments ,arguments :timeout 0.5)))
+				(menu-items (split-string res "\n"))
+				(menu `("Eglot code actions:" ("dummy" ,@menu-items)))
+				(db (if (listp last-nonmenu-event)
+					(x-popup-menu last-nonmenu-event menu)
+					(completing-read "[eglot] Pick an database: "
+							 menu-items nil t
+							 nil nil (car menu-items))
+					))
+				)
+			   (jsonrpc-request server :workspace/executeCommand
+					    `(:command "switchDatabase" :arguments [,db] :timeout 0.5))))
 	   (setq sql-connection-alist
 		 '((uhm-campus-energy
 		    (sql-product 'postgres)
