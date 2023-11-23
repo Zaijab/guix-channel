15b8a98623f52f57bcefddc47d87858c46cb57cb
Zain Jabbar <zaijab2000@gmail.com>
2023-11-22 18:08:08 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -54,6 +54,7 @@
 			"intel_iommu=on"
 			"pcie_acs_override=downstream,multifunction"
 			"vfio-pci.ids=10de:1e89,10de:10f8,10de:1ad8,10de,1ad9"
+			"blacklist nvidia"
 			))
     
     (locale "en_US.utf8")
