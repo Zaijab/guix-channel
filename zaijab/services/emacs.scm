1a368c99671832c86572c9aabcb8326c7a706ae6
Zain Jabbar <zaijab2000@gmail.com>
2024-02-24 14:30:56 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1918,7 +1918,8 @@ nil nil (car menu-items))
 
 (define home-emacs-total-configuration
   (fold (lambda (config-1 config-2) (home-emacs-configuration
-				     (emacs (specification->package "emacs-xwidgets"))
+				     (emacs ((options->transformation '((with-input . "emacs=emacs-next")))
+					     (specification->package "emacs-xwidgets")))
 				     (init (append (home-emacs-configuration-init config-1)
 						   (home-emacs-configuration-init config-2)))
 				     (early-init (append (home-emacs-configuration-early-init config-1)
