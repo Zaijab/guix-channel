bafd534330d3d48b60f297c8b66a1219f528191a
Zain Jabbar <zaijab2000@gmail.com>
2024-03-30 01:57:11 -1000

    Updating Config

--- zaijab/packages/inferior-guix.scm
+++ zaijab/packages/inferior-guix.scm
@@ -8,10 +8,10 @@
   #:use-module (srfi srfi-1)
   )
 
-(first (lookup-inferior-packages
-	(inferior-for-channels
-	 (list (channel
-		(name 'guix)
-		(url "https://git.savannah.gnu.org/git/guix.git")
-		(commit "8e2f32cee982d42a79e53fc1e9aa7b8ff0514714"))))
-	"webkitgtk-with-libsoup2"))
+;; (first (lookup-inferior-packages
+;; 	(inferior-for-channels
+;; 	 (list (channel
+;; 		(name 'guix)
+;; 		(url "https://git.savannah.gnu.org/git/guix.git")
+;; 		(commit "8e2f32cee982d42a79e53fc1e9aa7b8ff0514714"))))
+;; 	"webkitgtk-with-libsoup2"))
