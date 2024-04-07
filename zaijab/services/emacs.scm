853f9e5c8dd8058c82833dd462c182d31d953c86
Zain Jabbar <zaijab2000@gmail.com>
2024-04-06 15:02:08 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -558,35 +558,6 @@ If WINDOW is t, redisplay pages in all windows."
 				("https://www.youtube.com/feeds/videos.xml?playlist_id=PL6763F57A61FE6FE8" lecture topology wildberger)))
 	   (require 'elfeed-tube)
 	   (elfeed-tube-setup)
-	   (add-hook 'elfeed-new-entry-hook (function elfeed-declickbait-entry))
-
-	   (defun elfeed-declickbait-entry (entry)
-	     (let ((title (elfeed-entry-title entry)))
-	       (setf (elfeed-meta entry :title)
-		     (elfeed-title-transform title))))
-
-	   (defun elfeed-title-transform (title)
-	     "Declickbait string TITLE."
-	     (let* ((trim "\\(?:\\(?:\\.\\.\\.\\|[!?]\\)+\\)")
-		    (arr (split-string (s-replace-regexp "[“”]" "\"" title) nil t trim))
-		    (s-table (copy-syntax-table)))
-	       (modify-syntax-entry ?\' "w" s-table)
-	       (with-syntax-table s-table
-				  (mapconcat (lambda (word)
-					       (cond
-						((member word '("“" "”"))
-						 "")
-						((member word '("AND" "OR" "IF" "ON" "IT" "TO"
-								"A" "OF" "VS" "IN" "FOR" "WAS"
-								"IS" "BE"))
-						 (downcase word))
-						((member word '("WE" "DAY" "HOW" "WHY" "NOW" "OLD"
-								"NEW" "MY" "TOO" "GOT" "GET" "THE"
-								"ONE" "DO" "YOU"))
-						 (capitalize word))
-						((> (length word) 3) (capitalize word))
-						(t word)))
-					     arr " "))))
 	   (setq-default elfeed-search-filter "-fun")
 	   (setq-default elfeed-search-title-max-width 100)
 	   (setq-default elfeed-search-title-min-width 100)
