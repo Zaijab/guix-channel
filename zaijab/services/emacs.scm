b975d500d05fdebf3efe6f430b7dd6e98e9ab8a2
Zain Jabbar <zaijab2000@gmail.com>
2024-04-20 11:18:48 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1683,9 +1683,6 @@ If WINDOW is t, redisplay pages in all windows."
 
 (define home-emacs-total-configuration
   (fold (lambda (config-1 config-2) (home-emacs-configuration
-				     ;(emacs ;emacs-next-xwidgets
-					    ;(specification->package "emacs-xwidgets")
-					    ;)
 				     (init (append (home-emacs-configuration-init config-1)
 						   (home-emacs-configuration-init config-2)))
 				     (early-init (append (home-emacs-configuration-early-init config-1)
