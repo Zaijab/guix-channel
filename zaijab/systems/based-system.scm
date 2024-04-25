773141eb63b37c1d901ee71234cc551f52ad9c1b
Zain Jabbar <zaijab2000@gmail.com>
2024-04-24 14:22:12 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -111,7 +111,8 @@
 	       ;; (service kernel-module-loader-service-type
                ;;          '("vfio_pci" "vfio" "vfio_iommu_type1" #;"vfio-pci.ids=10de:1e89,10de:10f8,10de:1ad8,10de,1ad9"))
 	       (service openssh-service-type)
-	       (service opendht-service-type)
+	       (service opendht-service-type
+			(opendht-configuration (peer-discovery? #t)))
 	       
 	       (service syncthing-service-type
 			(syncthing-configuration (user "zjabbar")))
