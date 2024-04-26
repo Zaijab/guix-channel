186c3f1fcfa2d66ce28fd686c06980d30c553297
Zain Jabbar <zaijab2000@gmail.com>
2024-04-25 20:50:00 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -96,16 +96,6 @@
 		 read-file-name-completion-ignore-case t
 		 read-buffer-completion-ignore-case t)
 	   ))))
-(define debbugs-configuration
-  (home-emacs-configuration
-   (packages (list (specification->package "emacs-orderless")))
-   (init '((setq completion-styles '(orderless basic)
-		 completion-category-overrides '((file (styles basic partial-completion))))
-	   (setq orderless-smart-case nil
-		 completion-ignore-case t
-		 read-file-name-completion-ignore-case t
-		 read-buffer-completion-ignore-case t)
-	   ))))
 
 (define vertico-configuration
   (home-emacs-configuration
@@ -218,6 +208,13 @@
 	   (add-to-list 'consult-buffer-sources 'consult--source-workspace)
 
 	   ))))
+
+(define citation-configuration
+  (home-emacs-configuration
+   (packages (list (specification->package "emacs-citar")
+		   (specification->package "emacs-citar-org-roam")))
+   (init '((marginalia-mode)))))
+
 
 (define buffer-configuration
 (home-emacs-configuration
@@ -499,22 +496,22 @@ If WINDOW is t, redisplay pages in all windows."
    (init '((setq elfeed-feeds '(("https://www.youtube.com/feeds/videos.xml?channel_id=UC2D2CMWXMOVWx7giW1n3LIg" health huberman)
 				("https://www.youtube.com/feeds/videos.xml?channel_id=UCe0TLA0EsQbE-MjuHXevj2A" health jeff)
 				
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCkFJBuwX2iPKCgCITXt2Bnw" fun fatguy)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCrTW8WZTlOZMvvn_pl1Lpsg" fun nicob)
-				("https://twitchrss.appspot.com/vod/nicob" fun nicob twitch)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCP9q8DRbsTDPhU4E0R3-1rA" fun league pekin)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCT0fBcIYwMsp6IRCm5E3eTA" fun league pekin)
-				("https://twitchrss.appspot.com/vod/pekinwoof" fun league pekin twitch)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCIkcvRgwGlzEtfGf7k2oL3g" fun league virkayu)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCc3cbGWviHbC1OLJKFDfogA" fun league virkayu)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCwE00vEJFzpO6j1rDJMLDfg" fun league virkayu)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCkaw-9Mo41X_N8sT15EyRzA" fun league eagz)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCu-3KO4dBHSuz-57j4RHTKw" fun league hidon)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCcWrPkUDJRSPqt4kAF9DVsA" fun league leo)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCOQe4ma4be9SZ1n8B2ijihQ" fun league rogue)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCF_b_1kpeajcy03cb36zCkQ" fun ow awkward)
-				("https://www.youtube.com/feeds/videos.xml?channel_id=UCzxgSHk0g-D-eErxMiDy9UA" fun ow awkward)
-				("https://twitchrss.appspot.com/vod/awkward" fun ow awkward twitch)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkFJBuwX2iPKCgCITXt2Bnw" fun fatguy)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCrTW8WZTlOZMvvn_pl1Lpsg" fun nicob)
+				;; ("https://twitchrss.appspot.com/vod/nicob" fun nicob twitch)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCP9q8DRbsTDPhU4E0R3-1rA" fun league pekin)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCT0fBcIYwMsp6IRCm5E3eTA" fun league pekin)
+				;; ("https://twitchrss.appspot.com/vod/pekinwoof" fun league pekin twitch)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCIkcvRgwGlzEtfGf7k2oL3g" fun league virkayu)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCc3cbGWviHbC1OLJKFDfogA" fun league virkayu)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCwE00vEJFzpO6j1rDJMLDfg" fun league virkayu)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkaw-9Mo41X_N8sT15EyRzA" fun league eagz)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCu-3KO4dBHSuz-57j4RHTKw" fun league hidon)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCcWrPkUDJRSPqt4kAF9DVsA" fun league leo)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCOQe4ma4be9SZ1n8B2ijihQ" fun league rogue)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCF_b_1kpeajcy03cb36zCkQ" fun ow awkward)
+				;; ("https://www.youtube.com/feeds/videos.xml?channel_id=UCzxgSHk0g-D-eErxMiDy9UA" fun ow awkward)
+				;; ("https://twitchrss.appspot.com/vod/awkward" fun ow awkward twitch)
 
 				("https://www.youtube.com/feeds/videos.xml?channel_id=UCYO_jab_esuFRV4b17AJtAw" math grant)
 				("https://www.youtube.com/feeds/videos.xml?channel_id=UCgBRykS2v-WV2YYUpR2V9jw" math allangles)
@@ -1070,6 +1067,7 @@ If WINDOW is t, redisplay pages in all windows."
 		    (specification->package "emacs-jinx"))
 
 		   (specification->package "hunspell-dict-en")
+		   (specification->package "hunspell-dict-jp")
 		   
 		   ))
    (init '((require 'org)
@@ -1085,6 +1083,7 @@ If WINDOW is t, redisplay pages in all windows."
 	   (keymap-global-set "M-$" (function jinx-correct))
 	   (keymap-global-set "C-M-$" (function jinx-languages))
 	   (setq ispell-alternate-dictionary "/home/zjabbar/.guix-home/profile/share/hunspell/en_US.dic")
+	   ;(setq ispell-alternate-dictionary "/home/zjabbar/.guix-home/profile/share/hunspell/.dic")
 	   ;(add-to-list 'vertico-multiform-categories
 		;	'(jinx grid (vertico-grid-annotate . 20)))
 
