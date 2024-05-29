15e88eb3c0e93c53e65c2fc5c06cf185c98f88fa
Zain Jabbar <zaijab2000@gmail.com>
2024-05-28 22:42:01 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -97,7 +97,7 @@
 	       (service syncthing-service-type (syncthing-configuration (user "zjabbar")))
 	       (service guix-home-service-type `(("zjabbar" ,zains-home)))
 	       (service connman-service-type)
-	      (service oci-container-service-type
+	      #;(service oci-container-service-type
                    (list
                     (oci-container-configuration
                      (image
