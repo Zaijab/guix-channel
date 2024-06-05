28acf49598d0400e5234397d89ca478e80e0526c
Zain Jabbar <zaijab2000@gmail.com>
2024-06-05 13:23:51 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -133,11 +133,11 @@
 			       (scheme-file "nonguix.pub" '(public-key 
 							   (ecc 
 							    (curve Ed25519)
-							    (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#))))
+							    (q \#C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#))))
 			       (scheme-file "bordeaux.pub" '(public-key
 							    (ecc
 							     (curve Ed25519)
-							     (q #89FBA276A976A8DE2A69774771A92C8C879E0F24614AAAAE23119608707B3F06#))))
+							     (q \#89FBA276A976A8DE2A69774771A92C8C879E0F24614AAAAE23119608707B3F06#))))
 			       %default-authorized-guix-keys)))))))))
 
 
