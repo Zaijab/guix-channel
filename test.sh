d2f6ce50df72e174bbc63119ebf3ff20fe4aca44
Zain Jabbar <zaijab2000@gmail.com>
2024-06-17 01:54:13 -1000

    Updating Config

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 image=$(guix system image /home/zjabbar/code/guix-channel/zaijab/systems/pinephone.scm --no-grafts --image-type=rock64-raw)
 cp $image /tmp/my-image.qcow2
 chmod +w /tmp/my-image.qcow2
-qemu-system-aarch64 -enable-kvm -hda /tmp/my-image.qcow2 -m 1000 \
-		    -bios $(guix build ovmf-x86-64)/share/firmware/ovmf_x64.bin
+qemu-system-aarch64 -enable-kvm -hda /tmp/my-image.qcow2
