bf517fce811536e019861f6eb73578bb49eeb9e8
Zain Jabbar <zaijab2000@gmail.com>
2024-06-21 18:00:00 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1583,12 +1583,12 @@ If WINDOW is t, redisplay pages in all windows."
   (home-emacs-configuration
    ;(package (list ()))
    (init '(
-					(set-face-attribute 'default nil :font "Iosevka-14")
-	   (set-fontset-font "fontset-default" 'tibetan "Iosevka-14")
-	   (set-fontset-font "fontset-default" 'symbol "Iosevka-14")
-	   (set-fontset-font "fontset-default" 'han "IPAmjMincho-14")
-	   (set-fontset-font "fontset-default" 'kana "IPAmjMincho-14")
-	   (set-fontset-font "fontset-default" 'cjk-misc "IPAmjMincho-14")
+	   (set-face-attribute 'default nil :font "Iosevka-14")
+	   (set-fontset-font nil 'tibetan "Iosevka-14")
+	   (set-fontset-font nil 'symbol "Iosevka-14")
+	   (set-fontset-font nil 'han "IPAmjMincho-14")
+	   (set-fontset-font nil 'kana "IPAmjMincho-14")
+	   (set-fontset-font nil 'cjk-misc "IPAmjMincho-14")
 	   
 	   ;; (set-fontset-font t 'tibetan "Iosevka-14" nil 'prepend)
 	   ;; (set-fontset-font t 'symbol "Iosevka-14" nil 'prepend)
