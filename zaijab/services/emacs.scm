e0039a2d8d6285a4312f7ad174de6ce0cbe135b8
Zain Jabbar <zaijab2000@gmail.com>
2024-06-21 19:54:56 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -798,8 +798,8 @@ If WINDOW is t, redisplay pages in all windows."
 			 :after org-roam ;; or :after org
 			 ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
 			 ;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-			 ;;  :hook (after-init . org-roam-ui-mode)
+			 ;;         if you don't care about startup time, use
+			 :hook (after-init . org-roam-ui-mode)
 			 :config
 			 (setq org-roam-ui-sync-theme t
 			       org-roam-ui-follow t
