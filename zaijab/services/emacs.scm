147919f7d068017d9fb701352531326f9218eed4
Zain Jabbar <zaijab2000@gmail.com>
2024-06-24 21:56:12 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1227,13 +1227,7 @@ If WINDOW is t, redisplay pages in all windows."
 						:todo
 						:timestamp))
 	   
-	   (setq org-agenda-files '(
-				    "/home/zjabbar/notes/20211224040925-todo.org"
-				    "/home/zjabbar/notes/20240111152239-krs_152_weight_training.org"
-				    "/home/zjabbar/notes/20240110135311-math_633_hangelbroek.org"
-				    "/home/zjabbar/notes/20240111151852-math_649d_younsi_holomorphic_dynamics.org"
-				    "/home/zjabbar/notes/20240105195957-math_699_gawlik.org"
-				    ))
+	   (setq org-agenda-files '("/home/zjabbar/notes"))
 	   (setq cdlatex-math-modify-alist
 		 '((?a "\\mathbf" nil t nil nil)
 		   (?b "\\mathbb" nil t nil nil)
