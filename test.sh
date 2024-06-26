1915cb83dc84bfad8f0eda0650902a2474264bbd
Zain Jabbar <zaijab2000@gmail.com>
2024-06-25 15:32:22 -1000

    Updating Config

--- test.sh
+++
@@ -1,4 +0,0 @@
-image=$(guix system image /home/zjabbar/code/guix-channel/zaijab/systems/pinephone.scm --no-grafts --image-type=rock64-raw)
-cp $image /tmp/my-image.qcow2
-chmod +w /tmp/my-image.qcow2
-qemu-system-aarch64 -enable-kvm -hda /tmp/my-image.qcow2
