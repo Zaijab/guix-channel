24912ef38c064ce16b4dbbed92efed2f0f97e629
Zain Jabbar <zaijab2000@gmail.com>
2024-06-26 02:42:12 -1000

    Updating Config

---
+++ test.scm
@@ -0,0 +1,42 @@
+(list (channel
+        (name 'zaijab)
+        (url "/home/zjabbar/code/guix-channel/")
+        (branch "main")
+        (commit
+          "fa1fc552b819dc68d47ca4209149b51eda779c0e"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "877ed8cc4eee26ddde3d7d200c19c370c6bf7cb1"))
+      (channel
+        (name 'guix-science)
+        (url "https://github.com/guix-science/guix-science.git")
+        (branch "master")
+        (commit
+          "12eba5aa0c5cbf357c476ef7872e0d562d054eff"))
+      (channel
+        (name 'guix-science-nonfree)
+        (url "https://github.com/guix-science/guix-science-nonfree.git")
+        (branch "master")
+        (commit
+          "671eca0fcbd8c4a4fc72c3c3edff18ef609fdada"))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+        (branch "master")
+        (commit
+          "218322bb556b96efadceb55989a7c963f1e8b274"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "41891ea973b9cd658a6cc42875f8c65953eb6e12"))
+      (channel
+        (name 'guix-past)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-past")
+        (branch "master")
+        (commit
+          "aab6148759476f0270bbe098919a978da45d7a2d")))
