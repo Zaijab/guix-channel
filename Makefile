8d5fc146ddfd43e9fcbd83be554ef412d06b8711
Zain Jabbar <zaijab2000@gmail.com>
2024-06-25 19:37:29 -1000

    Updating Config

--- Makefile
+++ Makefile
@@ -35,6 +35,7 @@ pinephone-qemu:
 	image=$(guix system image /home/zjabbar/code/guix-channel/zaijab/systems/pinephone.scm --no-grafts --image-type=rock64-raw)
 	cp $image /tmp/my-image.qcow2
 	chmod +w /tmp/my-image.qcow2
+	guix shell qemu -- \
 	qemu-system-aarch64 -enable-kvm -hda /tmp/my-image.qcow2 -m 1000 \
 		-bios $(guix build ovmf-x86-64)/share/firmware/ovmf_x64.bin
 
--- zaijab/systems/pinephone.scm
+++ zaijab/systems/pinephone.scm
@@ -159,7 +159,7 @@
     (services (cons*
 	       (service wpa-supplicant-service-type)
 	       (service openssh-service-type)
-	       (service syncthing-service-type (syncthing-configuration (user "zjabbar")))
+	       ;; (service syncthing-service-type (syncthing-configuration (user "zjabbar")))
 	       ;(service guix-home-service-type `(("zjabbar" ,zains-home)))
 	       (service connman-service-type)
 	       (service tlp-service-type)
