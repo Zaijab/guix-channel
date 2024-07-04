bc54adb4b0c4435443ffbd9b77401fd93a1ae4b4
Zain Jabbar <zaijab2000@gmail.com>
2024-07-03 19:22:07 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -887,7 +887,7 @@ If WINDOW is t, redisplay pages in all windows."
 			(org-roam-ui-sync-theme t)
 			(org-roam-ui-follow t)
 			(org-roam-ui-update-on-save t)
-			(org-roam-ui-open-on-start t))
+			(org-roam-ui-open-on-start nil))
 	   
 	   
 
