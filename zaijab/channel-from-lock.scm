044abec900682d395fb55513a5aad1e27ac5f554
Zain Jabbar <zaijab2000@gmail.com>
2024-07-08 15:35:56 -1000

    Updating Config

--- zaijab/channel-from-lock.scm
+++ zaijab/channel-from-lock.scm
@@ -3,17 +3,17 @@
   #:use-module (rnrs io ports)
   #:use-module (guix channels))
 
-(define raw-data
-  (let* ((input+output (pipe))
-	 (pid (spawn "guix" '("guix" "describe" "-f" "channels")
-                     #:output (cdr input+output))))
-    (close-port (cdr input+output))
-    (read (open-string-input-port (get-string-all (car input+output))))))
+;; (define raw-data
+;;   (let* ((input+output (pipe))
+;; 	 (pid (spawn "guix" '("guix" "describe" "-f" "channels")
+;;                      #:output (cdr input+output))))
+;;     (close-port (cdr input+output))
+;;     (read (open-string-input-port (get-string-all (car input+output))))))
 
-(define master-zaijab-channel-lock
-  (cons*
-   '(channel (name 'zaijab)
-	     (url "/home/zjabbar/code/guix-channel/"))
-   (cddr raw-data)))
+;; (define master-zaijab-channel-lock
+;;   (cons*
+;;    '(channel (name 'zaijab)
+;; 	     (url "/home/zjabbar/code/guix-channel/"))
+;;    (cddr raw-data)))
 
-master-zaijab-channel-lock
+;; master-zaijab-channel-lock
