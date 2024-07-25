3906cdc5e0713b7775511e5ac9d89402ddf6e3d4
Zain Jabbar <zaijab2000@gmail.com>
2024-07-24 20:40:53 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -46,6 +46,60 @@
   #:use-module (gnu services virtualization)
   #:use-module (zaijab home zjabbar))
 
+(define-public main-services
+  (cons*
+   (service unattended-upgrade-service-type)
+   (service openssh-service-type)
+   (service syncthing-service-type (syncthing-configuration (user "zjabbar")))
+   (service guix-home-service-type `(("zjabbar" ,zains-home)))
+   (service connman-service-type)
+   (service tlp-service-type)
+   (service docker-service-type)
+   (service oci-container-service-type
+	    (list
+	     (oci-container-configuration
+	      (image "docker.io/library/caddy:2-alpine")
+	      (network "host")
+	      (volumes '("/home/zjabbar/code/guix-channel/zaijab/files/Caddyfile:/etc/caddy/Caddyfile:ro"
+			 "/var/run/caddy-data:/data:rw"
+			 "/var/run/caddy-config:/config:rw"))
+	      (environment '("LETSENCRYPT_EMAIL=zaijab2000@gmail.com"
+			     "SEARXNG_HOSTNAME=http://localhost:8080"
+			     "SEARXNG_TLS=zaijab2000@gmail.com")))
+	     (oci-container-configuration
+	      (image "docker.io/valkey/valkey:7-alpine")
+	      (network "host")
+	      (volumes '("/var/run/valkey-data2:/data")))
+	     (oci-container-configuration
+	      (image "docker.io/searxng/searxng")
+	      (network "host")
+	      (ports '(("8080" . "8080")))
+	      (volumes '("/var/run/searxng:/etc/searxng:rw"
+			 "/home/zjabbar/code/guix-channel/zaijab/files/limiter.toml:/etc/searxng/limiter.toml"
+			 "/home/zjabbar/code/guix-channel/zaijab/files/settings.yml:/etc/searxng/settings.yml"))
+	      (environment '(("SEARXNG_BASE_URL" . "http://localhost:8080"))))))
+
+   (modify-services %desktop-services
+     (delete pulseaudio-service-type)
+     (delete gdm-service-type)
+     (delete network-manager-service-type)
+     (mingetty-service-type
+      config => (mingetty-configuration
+		 (inherit config)
+		 (auto-login "zjabbar")
+		 (login-pause? #t)))
+     (guix-service-type
+      config => (guix-configuration
+		 (inherit config)
+		 (substitute-urls
+		  (cons* "https://substitutes.nonguix.org"
+			 "https://guix.bordeaux.inria.fr"
+			 %default-substitute-urls))
+		 (authorized-keys
+		  (cons* (plain-file "nonguix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))")
+			 (plain-file "bordeaux.pub" "(public-key (ecc (curve Ed25519) (q #89FBA276A976A8DE2A69774771A92C8C879E0F24614AAAAE23119608707B3F06#)))")
+			 %default-authorized-guix-keys)))))))
+
 (define-public tao-operating-system
   (operating-system
     (kernel linux)
@@ -91,58 +145,15 @@
 		  (supplementary-groups '("wheel" "netdev" "audio" "lp" "video" "docker")))
 		 %base-user-accounts))
     
-    (services (cons*
-	       (service unattended-upgrade-service-type)
-	       (service openssh-service-type)
-	       (service syncthing-service-type (syncthing-configuration (user "zjabbar")))
-	       (service guix-home-service-type `(("zjabbar" ,zains-home)))
-	       (service connman-service-type)
-	       (service tlp-service-type)
-	       (service docker-service-type)
-	       (service oci-container-service-type
-			(list
-			 (oci-container-configuration
-			  (image "docker.io/library/caddy:2-alpine")
-			  (network "host")
-			  (volumes '("/home/zjabbar/code/guix-channel/zaijab/files/Caddyfile:/etc/caddy/Caddyfile:ro"
-				     "/var/run/caddy-data:/data:rw"
-				     "/var/run/caddy-config:/config:rw"))
-			  (environment '("LETSENCRYPT_EMAIL=zaijab2000@gmail.com"
-					 "SEARXNG_HOSTNAME=http://localhost:8080"
-					 "SEARXNG_TLS=zaijab2000@gmail.com")))
-			 (oci-container-configuration
-			  (image "docker.io/valkey/valkey:7-alpine")
-			  (network "host")
-			  (volumes '("/var/run/valkey-data2:/data")))
-			 (oci-container-configuration
-			  (image "docker.io/searxng/searxng")
-			  (network "host")
-			  (ports '(("8080" . "8080")))
-			  (volumes '("/var/run/searxng:/etc/searxng:rw"
-				     "/home/zjabbar/code/guix-channel/zaijab/files/limiter.toml:/etc/searxng/limiter.toml"
-				     "/home/zjabbar/code/guix-channel/zaijab/files/settings.yml:/etc/searxng/settings.yml"))
-			  (environment '(("SEARXNG_BASE_URL" . "http://localhost:8080"))))))
-
-	       (modify-services %desktop-services
-		 (delete pulseaudio-service-type)
-		 (delete gdm-service-type)
-		 (delete network-manager-service-type)
-		 (mingetty-service-type
-		  config => (mingetty-configuration
-			     (inherit config)
-			     (auto-login "zjabbar")
-			     (login-pause? #t)))
-		 (guix-service-type
-		  config => (guix-configuration
-			     (inherit config)
-			     (substitute-urls
-			      (cons* "https://substitutes.nonguix.org"
-				     "https://guix.bordeaux.inria.fr"
-				     %default-substitute-urls))
-			     (authorized-keys
-			      (cons* (plain-file "nonguix.pub" "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))")
-				     (plain-file "bordeaux.pub" "(public-key (ecc (curve Ed25519) (q #89FBA276A976A8DE2A69774771A92C8C879E0F24614AAAAE23119608707B3F06#)))")
-				     %default-authorized-guix-keys)))))))))
+    (services (cons* (simple-service 'vfio-override
+				      boot-service-type
+				      '(and (call-with-output-file "/sys/bus/pci/devices/0000:04:00.0/driver_override"
+									       (lambda (p)
+										 (display "vfio-pci" p)))
+									     (call-with-output-file "/sys/bus/pci/drivers/vfio-pci/new_id"
+									       (lambda (p)
+										 (display "1002 164c" p)))))
+				      main-services))))
 
 
 (define-public euler-operating-system
