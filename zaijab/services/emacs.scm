ca70d6fa9e91ebbc087cb3ef8ed9646da258619b
Zain Jabbar <zaijab2000@gmail.com>
2024-08-12 16:28:09 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -721,7 +721,7 @@ If WINDOW is t, redisplay pages in all windows."
 	     (interactive)
 	     (emms-player-mpv-cmd (list 'add 'volume (- (or amount '10))))
 	     (emms-player-mpv-get-volume))
-	   (emms-add-directory-tree "~/music/persona_3_reload/")
+	   (emms-add-directory-tree "~/music/random/")
 	   (emms-shuffle)
 	   (emms-player-mpv-lower-volume 30)
 
