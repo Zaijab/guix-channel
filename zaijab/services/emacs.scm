732b71d65990cfb1e0a0be1ed87298eefa97d7d0
Zain Jabbar <zaijab2000@gmail.com>
2024-08-15 23:05:35 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1701,7 +1701,8 @@ Valid contexts:
 
 (define ui-configuration
   (home-emacs-configuration
-   (packages (list emacs-rainbow-delimiters))
+   (packages (list emacs-rainbow-delimiters
+		   emacs-explain-pause-mode))
    (early-init '((setq gc-cons-threshold 800000
 		       package-enable-at-startup nil
 		       indicate-empty-lines nil
