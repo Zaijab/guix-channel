fa299ca21f26881e7ba2542a96ba56c448fdeb39
Zain Jabbar <zaijab2000@gmail.com>
2024-09-02 11:34:41 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -164,7 +164,7 @@
 			       ("C-c k" . consult-kmacro)
 			       ("C-c m" . consult-man)
 			       ("C-c i" . consult-info)
-			       ([remap Info-search] . consult-info)
+			       ("<remap> <Info-search>" . consult-info)
 			       ;; C-x bindings in `ctl-x-map'
 			       ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
 			       ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
