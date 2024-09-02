d2a0f0880fd99f72e867a8baadc17cd23c5a0f2b
Zain Jabbar <zaijab2000@gmail.com>
2024-09-02 13:13:07 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -383,19 +383,43 @@ See `consult-grep' for details."
 (define corfu-configuration
   (home-emacs-configuration
    (packages (list emacs-corfu))
-   (init '((global-corfu-mode)
-	   (corfu-history-mode)
-	   (setq corfu-cycle t
-		 corfu-auto t
-		 corfu-auto-prefix 2
-		 corfu-auto-delay 0.0
-		 corfu-quit-at-boundary 'separator
-		 corfu-echo-documentation 0.25
-		 corfu-preview-current 'insert
-		 corfu-preselect-first nil)
-	   (define-key corfu-map (kbd "M-<SPC>") (function corfu-insert-separator))
-	   (define-key corfu-map (kbd "M-}") (function corfu-next))
-	   (define-key corfu-map (kbd "M-{") (function corfu-previous))))))
+   (init '((use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary 'separator)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
+	   ;; (global-corfu-mode)
+	   ;; (corfu-history-mode)
+	   ;; (setq corfu-cycle t
+	   ;; 	 corfu-auto t
+	   ;; 	 corfu-auto-prefix 2
+	   ;; 	 corfu-auto-delay 0.0
+	   ;; 	 corfu-quit-at-boundary 'separator
+	   ;; 	 corfu-echo-documentation 0.25
+	   ;; 	 corfu-preview-current 'insert
+	   ;; 	 corfu-preselect-first nil)
+	   ;; (define-key corfu-map (kbd "M-<SPC>") (function corfu-insert-separator))
+	   ;; (define-key corfu-map (kbd "M-}") (function corfu-next))
+	   ;; (define-key corfu-map (kbd "M-{") (function corfu-previous))
+	   ))))
 
 ;; Templates
 (define tempel-configuration
