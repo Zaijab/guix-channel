3f95535745cd08cbbf355b9607ea36cb70eb727b
Zain Jabbar <zaijab2000@gmail.com>
2024-10-21 15:23:20 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -2030,7 +2030,7 @@ See `consult-grep' for details."
 			     (hash-map->list (lambda (x y) y) (struct-ref (current-module) 0)))))))
 
 (define (use-emacs-next package)
-  (if (or #t #;(eq? package emacs-citar-org-roam))
+  (if (or #f #;(eq? package emacs-citar-org-roam))
       package
       ((options->transformation '((with-input . "emacs=emacs-next")
 				  (with-input . "emacs-minimal=emacs-next")))
