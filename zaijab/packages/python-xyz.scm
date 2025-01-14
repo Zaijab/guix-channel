30988659f9051ddc23a844e75004b1dfd8371b90
Zain Jabbar <zaijab2000@gmail.com>
2025-01-13 23:20:37 -1000

    Updating Config

--- zaijab/packages/python-xyz.scm
+++ zaijab/packages/python-xyz.scm
@@ -23,71 +23,71 @@
   #:use-module (guix build-system pyproject)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public pass-import
-  (package
-    (name "pass-import")
-    (version "3.5")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/roddhjav/pass-import")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0xj8nh1jcmgsllykzhg4ybxfyjxzpwqsxl9sl86965247arkgw9r"))))
-    (build-system python-build-system)
-    (arguments
-     (list
-      ;; WARNING: 21/146 tests have errors
-      ;; I think they're due to path issues,
-      ;; and don't seem to impact operation
-      #:tests? #f
-      #:phases #~(modify-phases %standard-phases
-                   (add-before 'build 'patch-base-and-share
-                     (lambda* (#:key inputs #:allow-other-keys)
-                       (substitute* "setup.py"
-                         (("^base = .*$")
-                          (string-append "base = '"
-                                         #$output "'\n"))
-                         (("^share = .*$")
-                          (string-append "share = Path('"
-                                         #$output "', 'share')\n")))))
-                   (add-after 'build 'make-docs
-                     (lambda* (#:key inputs #:allow-other-keys)
-                       (invoke "pandoc"
-                               "-t"
-                               "man"
-                               "-s"
-                               "-o"
-                               "share/man/man1/pass-import.1"
-                               "share/man/man1/pass-import.md")
-                       (invoke "pandoc"
-                               "-t"
-                               "man"
-                               "-s"
-                               "-o"
-                               "share/man/man1/pimport.1"
-                               "share/man/man1/pimport.md"))))))
-    (native-inputs (list pandoc))
-    (propagated-inputs (list python-pyaml
-                             python-zxcvbn
-                             python-requests
-                             python-pykeepass
-                             python-secretstorage
-                             python-defusedxml
-                             python-secretstorage
-                             python-cryptography
-                             password-store))
-    (home-page "https://github.com/roddhjav/pass-import")
-    (synopsis
-     "Pass extension to import passwords from other password managers")
-    (description
-     "Pass import is a password store extension allowing you to
-import your password database to a password store repository conveniently.  It
-natively supports import from 62 different password managers.  More manager
-support can easily be added.")
-    (license license:gpl3)))
+;; (define-public pass-import
+;;   (package
+;;     (name "pass-import")
+;;     (version "3.5")
+;;     (source
+;;      (origin
+;;        (method git-fetch)
+;;        (uri (git-reference
+;;              (url "https://github.com/roddhjav/pass-import")
+;;              (commit (string-append "v" version))))
+;;        (file-name (git-file-name name version))
+;;        (sha256
+;;         (base32 "0xj8nh1jcmgsllykzhg4ybxfyjxzpwqsxl9sl86965247arkgw9r"))))
+;;     (build-system python-build-system)
+;;     (arguments
+;;      (list
+;;       ;; WARNING: 21/146 tests have errors
+;;       ;; I think they're due to path issues,
+;;       ;; and don't seem to impact operation
+;;       #:tests? #f
+;;       #:phases #~(modify-phases %standard-phases
+;;                    (add-before 'build 'patch-base-and-share
+;;                      (lambda* (#:key inputs #:allow-other-keys)
+;;                        (substitute* "setup.py"
+;;                          (("^base = .*$")
+;;                           (string-append "base = '"
+;;                                          #$output "'\n"))
+;;                          (("^share = .*$")
+;;                           (string-append "share = Path('"
+;;                                          #$output "', 'share')\n")))))
+;;                    (add-after 'build 'make-docs
+;;                      (lambda* (#:key inputs #:allow-other-keys)
+;;                        (invoke "pandoc"
+;;                                "-t"
+;;                                "man"
+;;                                "-s"
+;;                                "-o"
+;;                                "share/man/man1/pass-import.1"
+;;                                "share/man/man1/pass-import.md")
+;;                        (invoke "pandoc"
+;;                                "-t"
+;;                                "man"
+;;                                "-s"
+;;                                "-o"
+;;                                "share/man/man1/pimport.1"
+;;                                "share/man/man1/pimport.md"))))))
+;;     (native-inputs (list pandoc))
+;;     (propagated-inputs (list python-pyaml
+;;                              python-zxcvbn
+;;                              python-requests
+;;                              python-pykeepass
+;;                              python-secretstorage
+;;                              python-defusedxml
+;;                              python-secretstorage
+;;                              python-cryptography
+;;                              password-store))
+;;     (home-page "https://github.com/roddhjav/pass-import")
+;;     (synopsis
+;;      "Pass extension to import passwords from other password managers")
+;;     (description
+;;      "Pass import is a password store extension allowing you to
+;; import your password database to a password store repository conveniently.  It
+;; natively supports import from 62 different password managers.  More manager
+;; support can easily be added.")
+;;     (license license:gpl3)))
 
 (define-public python-tinytag
   (package
@@ -110,20 +110,20 @@ support can easily be added.")
 files.")
     (license license:expat)))
 
-(define-public python-equinox
-  (package
-    (name "python-equinox")
-    (version "0.11.7")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "equinox" version))
-       (sha256
-        (base32 "1gjilam3mnw72995zib2gmzgndjan6xby32v8sqw8bl2kmm23q4n"))))
-    (build-system pyproject-build-system)
-    (propagated-inputs (list python-jax python-jaxtyping
-                             python-typing-extensions python-hatchling))
-    (home-page "")
-    (synopsis "Elegant easy-to-use neural networks in JAX.")
-    (description "Elegant easy-to-use neural networks in JAX.")
-    (license #f)))
+;; (define-public python-equinox
+;;   (package
+;;     (name "python-equinox")
+;;     (version "0.11.7")
+;;     (source
+;;      (origin
+;;        (method url-fetch)
+;;        (uri (pypi-uri "equinox" version))
+;;        (sha256
+;;         (base32 "1gjilam3mnw72995zib2gmzgndjan6xby32v8sqw8bl2kmm23q4n"))))
+;;     (build-system pyproject-build-system)
+;;     (propagated-inputs (list python-jax python-jaxtyping
+;;                              python-typing-extensions python-hatchling))
+;;     (home-page "")
+;;     (synopsis "Elegant easy-to-use neural networks in JAX.")
+;;     (description "Elegant easy-to-use neural networks in JAX.")
+;;     (license #f)))
