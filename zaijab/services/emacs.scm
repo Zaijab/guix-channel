cf0728fba99c2413bd4c5144763a55ab537377cb
Zain Jabbar <zaijab2000@gmail.com>
2025-02-06 17:53:18 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -2141,7 +2141,7 @@ See `consult-grep' for details."
 (define (use-emacs-next package)
   (if #t
       ((options->transformation '((with-git-url . "emacs-org=https://code.tecosaur.net/tec/org-mode.git")
-				  (with-commit . "emacs-org=119426b1dde3f5bf9a1b180fe4344fc31ae82f9e")
+				  (with-commit . "emacs-org=master") ;119426b1dde3f5bf9a1b180fe4344fc31ae82f9e
 				  (without-tests . "emacs-org")))
        package)
       package))
