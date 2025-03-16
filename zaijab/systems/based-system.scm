b91efc7d894e0225c431e67ae79331ad0e1bbede
Zain Jabbar <zaijab2000@gmail.com>
2025-03-15 20:52:03 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -191,19 +191,19 @@
   (operating-system
     (inherit based-operating-system)
     (kernel-arguments (cons* "module_blacklist=pcspkr,snd_pcsp"
-			     ;"modprobe.blacklist=nouveau"
-			     ;"nvidia_drm.fbdev=1"
-			     ;"nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+			     "modprobe.blacklist=nouveau"
+			     "nvidia_drm.fbdev=1"
+			     "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
 			     %default-kernel-arguments))
     
     (host-name "tao")
     
     (services (cons*
-	       ;; (service nvidia-service-type)
-	       ;; (set-xorg-configuration
-	       ;; 	(xorg-configuration
-	       ;; 	 (modules (cons nvda %default-xorg-modules))
-	       ;; 	 (drivers '("nvidia"))))
+	       (service nvidia-service-type)
+	       (set-xorg-configuration
+		(xorg-configuration
+		 (modules (cons nvda %default-xorg-modules))
+		 (drivers '("nvidia"))))
 	       main-services))
     ))
 
