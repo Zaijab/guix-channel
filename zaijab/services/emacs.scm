2d09a11df25f7ac757a163acc14d1d6671515288
Zain Jabbar <zaijab2000@gmail.com>
2025-04-17 19:48:49 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -2219,7 +2219,7 @@ END is the start of the line with :END: on it."
 
 
 (define (use-emacs-next package)
-  (if #f
+  (if #t
       ((options->transformation '((with-commit . "emacs-org-roam=0b9fcbc97b65b349826e63bad89ca121a08fd2be")
 				  (with-git-url . "emacs-org=https://code.tecosaur.net/tec/org-mode.git")
 				  (with-commit . "emacs-org=cd2269ddb64bda7203acf2ee2e26188237a578ea")
