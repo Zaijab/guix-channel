aaa418bfb9585c2ffbfec7dd26ee07aec39c7c7c
Zain Jabbar <zaijab2000@gmail.com>
2025-04-18 13:55:01 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -2235,7 +2235,7 @@ END is the start of the line with :END: on it."
 ;;       package))
 
 (define (use-emacs-next package)
-  (if #t
+  (if #f
       ((options->transformation '((with-commit . "emacs-org-roam=64e302c1269a1a16c4c0b5a7d1e3baf2d5ded174")))
        package)
       package))
