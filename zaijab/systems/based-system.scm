a433ede3ca9f0850949da8178dfc24f2f25db2c1
Zain Jabbar <zaijab2000@gmail.com>
2025-04-29 14:48:21 -1000

    Updating Config

--- zaijab/systems/based-system.scm
+++ zaijab/systems/based-system.scm
@@ -103,7 +103,7 @@
             (cups-configuration
              (web-interface? #t)
              (extensions
-              (list cups-filters epson-inkjet-printer-escpr hplip-minimal hplip hplip-plugin))))
+              (list cups-filters epson-inkjet-printer-escpr foo2zjs hplip-minimal hplip hplip-plugin))))
 
 
    (modify-services %desktop-services
