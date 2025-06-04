b86cd5744b7c6af390c284baba8dfe82f43c14fd
Zain Jabbar <zaijab2000@gmail.com>
2025-06-03 16:34:56 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1924,7 +1924,8 @@ END is the start of the line with :END: on it."
 
 (define shell-configuration
   (home-emacs-configuration
-   (packages (list emacs-eat))
+   (packages (list emacs-eat
+		   emacs-bash-completion))
    (init '(#;(use-package eat
 	   :demand t
 	   :hook
