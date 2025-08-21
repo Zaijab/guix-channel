253d4826df1d6f8e360327b68ebee7c4fb58fdfa
Zain Jabbar <zaijab2000@gmail.com>
2025-08-20 14:49:19 -1000

    Updating Config

--- test.sh
+++
@@ -1,6 +0,0 @@
-git update-ref refs/heads/main b11a4eb8bbda6b593dc3c0552abaea5a19458c7f
-echo "ref: refs/heads/main" > .git/HEAD
-mv .git/index .git/index.bad
-git reset
-git add -A
-git commit -m "Recover work after crash"
