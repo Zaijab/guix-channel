6dfafbfb654ea025d0dcf922104e33f97a346631
Zain Jabbar <zaijab2000@gmail.com>
2025-09-12 14:47:02 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -66,7 +66,7 @@
   #:use-module (zaijab packages emacs-xyz)
   #:use-module (zaijab packages python-xyz)
   #:use-module (nongnu packages emacs)
-  #:use-module (ruther packages ripgrep-all)
+  ;; #:use-module (ruther packages ripgrep-all)
   #:use-module (nongnu packages fonts)
   #:use-module (nongnu packages nvidia)
   #:use-module (guix-science packages python)
