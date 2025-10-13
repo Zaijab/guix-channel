1ebce68b9c2c3ac8e6a0acbed751135f433e1bcb
Zain Jabbar <zaijab2000@gmail.com>
2025-10-13 13:18:54 -1000

    Updating Config

--- zaijab/packages/mathematics.scm
+++ zaijab/packages/mathematics.scm
@@ -377,7 +377,7 @@ include Groebner bases and free resolutions of modules over polynomial rings,
 Ext, Tor, and local cohomology.")
     (license license:gpl2+)))
 
-(define-public macaulay2
+(define-public macaulay2-working-with-shell
   (package
     (name "macaulay2")
     (version "1.25.06")
@@ -576,5 +576,182 @@ include Groebner bases and free resolutions of modules over polynomial rings,
 Ext, Tor, and local cohomology.")
     (license license:gpl2+)))
 
+
+(define-public macaulay2
+  (package
+    (name "macaulay2")
+    (version "1.25.06")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/Macaulay2/M2.git")
+             (commit "6fe351dcfe733f2a525d361d8585f9f2375e264d")
+             (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1960sa9936s37nassa7cz004ailx24iw9ckbn583i45xi6hl73pr"))))
+
+    (build-system cmake-build-system)
+    
+    (arguments
+     (list
+      #:tests? #f
+      
+      #:configure-flags
+      #~(list 
+	 "-GNinja"
+	 "-DPARALLEL_JOBS=1"
+	 "-DCMAKE_BUILD_TYPE=Release"
+	 "-DCMAKE_CXX_FLAGS=-O1 -g0 -DNDEBUG"
+	 (string-append "-DREADLINE_INCLUDE_DIR=" 
+			#$(this-package-input "readline") "/include")
+	 "-DCONFIGURE_COMMAND=${CONFIGURE_COMMAND};--disable-documentation")
+
+      #:phases
+      #~(modify-phases %standard-phases
+
+	  (add-before 'build 'fix-shebangs-and-shell
+	    (lambda _
+	      (setenv "SHELL" (string-append #$bash-minimal "/bin/bash"))
+
+	      (substitute* "build.ninja"
+		(("autoreconf -vif &&")
+		 (string-append
+		  "autoreconf -vif && "
+		  "sed -i '1s|^#!.*|#!" #$(file-append bash-minimal "/bin/bash") "|' configure && "
+		  "sed -i 's|/bin/sh|" #$(file-append bash-minimal "/bin/bash") "|g' configure &&")))))
+
+	  (add-before 'build 'force-make-shell
+	    (lambda _
+	      (setenv "MAKEFLAGS"
+		      (string-append "SHELL=" #$bash-minimal "/bin/bash"))))
+
+          (add-after 'unpack 'enter-m2-directory
+            (lambda _
+              (chdir "M2")))
+          
+          (add-after 'enter-m2-directory 'setup-build-directory
+            (lambda _
+              (mkdir-p "BUILD/build")
+              (chdir "BUILD/build")))
+          
+	  (add-after 'setup-build-directory 'setup-external-sources
+	    (lambda* (#:key inputs #:allow-other-keys)
+	      (mkdir-p "../tarfiles")
+	      (let ((nauty-source (assoc-ref inputs "nauty-source"))
+		    (gfan-source (assoc-ref inputs "gfan-source")))
+		(when nauty-source
+		  (copy-file nauty-source "../tarfiles/nauty2_8_9.tar.gz"))
+		(when gfan-source
+		  (copy-file gfan-source "../tarfiles/gfan0.6.2.tar.gz")))))
+	  
+          (replace 'configure
+            (lambda* (#:key configure-flags #:allow-other-keys)
+              (apply invoke "cmake" "-S../.." "-B." configure-flags)))
+
+	  (replace 'build
+	    (lambda _
+	      (setenv "CheckDocumentation" "false")
+	      (setenv "IgnoreExampleErrors" "true")
+	      (setenv "RemakeAllDocumentation" "false")
+	      (setenv "RerunExamples" "false")
+	      
+	      (invoke "ninja" "build-libraries")
+	      (invoke "ninja" "M2-binary" "M2-core")
+	      (invoke "ninja" "build-programs")))
+
+	  (replace 'install
+  (lambda* (#:key outputs #:allow-other-keys)
+    (let* ((out (assoc-ref outputs "out"))
+           (bin (string-append out "/bin"))
+           (share (string-append out "/share"))
+           (m2-root (string-append out "/x86_64-Linux-Linux-6.16.9"))
+           (m2-bin (string-append m2-root "/bin/M2")))
+      (copy-recursively "usr-dist" out)
+      
+      (mkdir-p bin)
+      (call-with-output-file (string-append bin "/M2")
+        (lambda (port)
+          (format port "#!~a/bin/bash~%exec ~a \"$@\"~%"
+                  #$bash-minimal m2-bin)))
+      (chmod (string-append bin "/M2") #o555)
+      
+      (mkdir-p (string-append share "/emacs/site-lisp"))
+      (symlink (string-append out "/common/share/emacs/site-lisp/macaulay2")
+               (string-append share "/emacs/site-lisp/macaulay2")))))
+	  
+	  )))
+
+    (native-inputs
+     `(("bash-minimal" ,bash-minimal)
+       ("cmake" ,cmake)
+       ("ninja" ,ninja)
+       ("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("gcc-toolchain" ,gcc-toolchain)
+       ("gcc-toolchain" ,gcc-toolchain)
+
+       ("gnu-make" ,gnu-make)
+       ("libtool" ,libtool)
+       ("pkg-config" ,pkg-config)
+       ("bison" ,bison)
+       ("python" ,python)
+       ("texlive-bin" ,texlive-bin)
+       ("cohomcalg" ,cohomcalg)
+       ("topcom" ,topcom)
+       ("nauty-source"
+	,(origin
+	   (method url-fetch)
+	   (uri "https://pallini.di.uniroma1.it/nauty2_8_9.tar.gz")
+	   (sha256
+            (base32 "1vn4abz498h8fbh27z0l5jrs4z04d693xklbb5mai5l7yhmv8yn9"))))
+       ("gfan-source"
+	,(origin
+	   (method url-fetch)
+	   (uri "https://users-math.au.dk/jensen/software/gfan/gfan0.6.2.tar.gz")
+	   (sha256
+	    (base32 "02pihqb1lb76a0xbfwjzs1cd6ay3ldfxsm8dvsbl6qs3vkjxax56"))))
+       ))
+    
+    (inputs
+     (list
+      singular
+      boost
+      gdbm
+      tbb
+      libffi
+      readline
+      gmp
+      libxml2
+      eigen
+      googletest
+      mpfr
+      ntl
+      glpk
+      mpfi
+      normaliz
+      libgc
+      lrslib
+      cddlib
+      4ti2
+      flint
+      fflas-ffpack
+      givaro
+      mpsolve
+      csdp
+      msolve
+      lapack
+      openblas))
+    (home-page "https://macaulay2.com")
+    (synopsis "Software system for research in algebraic geometry and commutative algebra")
+    (description
+     "Macaulay2 is a software system devoted to supporting research in
+algebraic geometry and commutative algebra.  This version uses the modern
+CMake build system and skips package installation for significantly faster
+build times while maintaining full mathematical functionality.  Features
+include Groebner bases and free resolutions of modules over polynomial rings,
+Ext, Tor, and local cohomology.")
+    (license license:gpl2+)))
 
 macaulay2
