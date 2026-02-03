2a444a016f9f3dc22f62fbce1b476036b6fa2fe3
Zain Jabbar <zaijab2000@gmail.com>
2026-02-02 15:07:55 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1922,7 +1922,7 @@ END is the start of the line with :END: on it."
 	      gcc-toolchain
 	      python-cython
 	      ;; conda
-	      ;; jupyter
+	      jupyter
 	      ;; guix-jupyter
 	      python-virtualenv
 
@@ -1967,7 +1967,7 @@ END is the start of the line with :END: on it."
 										 (sql . t)
 										 (eshell . t)
 										 (shell . t)
-										 (jupyter . t)
+										 ;(jupyter . t)
 										 ))
 
 			(add-to-list 'org-src-lang-modes (cons "python3" 'python))
