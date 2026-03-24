0fcd3aa07e7bd2b61a23596e82b5690db4decf5c
Zain Jabbar <zaijab2000@gmail.com>
2026-03-23 15:34:28 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -1735,7 +1735,8 @@ END is the start of the line with :END: on it."
 	      emacs-calfw-blocks-compatible
 	      emacs-calfw-blocks
 	      dvisvgm
-	      texlive-bin
+	      texlive
+	      ;; texlive-bin
 	      ;; texlive-collection-latexrecommended
 	      ;; texlive-collection-latexextra
 	      ;; texlive-collection-fontsrecommended
