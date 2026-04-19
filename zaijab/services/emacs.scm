54e7c549bbaee6629513b364a8a816bd388a3960
Zain Jabbar <zaijab2000@gmail.com>
2026-04-19 12:49:46 -1000

    Updating Config

--- zaijab/services/emacs.scm
+++ zaijab/services/emacs.scm
@@ -2415,6 +2415,7 @@ END is the start of the line with :END: on it."
 	    	       initial-scratch-message nil
 		       inhibit-compacting-font-caches t
 		       ;; native-comp-speed 0
+		       native-comp-jit-compilation t
 	    	       large-file-warning-threshold 1000000000)
 		 (setq-default abbrev-mode t)
 		 (set-face-attribute 'mode-line nil :box nil)
