cf0096338896e35f3ed56dbc1373ca01116812e3
Zain Jabbar <zaijab2000@gmail.com>
2026-09-17 14:14:34 -1000

    Updating Config

--- zaijab/files/startx.sh
+++ zaijab/files/startx.sh
@@ -5,6 +5,6 @@ source ~/.bash_profile
 DIR=$HOME/.guix-home/profile
 SESSION=/home/zjabbar/code/guix-channel/zaijab/files/xsession
 
-$DIR/bin/xinit -- $DIR/bin/Xorg :0 vt1 -keeptty \
+$DIR/bin/xinit "$SESSION" -- $DIR/bin/Xorg :0 vt1 -keeptty \
                 -configdir $DIR/share/X11/xorg.conf.d \
                 -modulepath $DIR/lib/xorg/modules
